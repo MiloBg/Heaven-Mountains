07f451f7cd23f22ae4d1a7c39896592271818427
MiloBg <68861551+MiloBg@users.noreply.github.com>
2020-10-23 14:43:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -238,10 +238,35 @@ $(function() {
 $(document).ready(function() {
     // Hamburger Menu animation
     $(".open-button").click(function() {
-        $(this).toggleClass(".open");
+        $(this).toggleClass("open");
     });
+    // Menu fade on mobile
     $(".open-button").click(function(e) {
         e.preventDefault();
-        $(".nav-menu").toggleClass(".open");
+        $(".nav-menu").toggleClass("open");
     });
+});
+
+$(document).ready(function() {
+    $(".nav-menu a").click(function() {
+        $(".nav-menu").toggleClass("open");
+        $(".open-button").removeClass("open");
+    });
+});
+
+/* Smooth Scroll */
+
+$(function() {
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
